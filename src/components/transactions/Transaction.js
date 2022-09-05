912a52a4568185ddd03befe5eb1bcccf6ac0808f
noormohammad20 <mdsaim204@gmail.com>
2022-09-06 00:28:09 +0600

    update transaction feture added

--- src/components/transactions/Transaction.js
+++ src/components/transactions/Transaction.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import editImage from '../../assets/images/edit.svg'
-import deleteImage from '../../assets/images/delete.svg'
+import { useDispatch } from "react-redux"
+import deleteImage from "../../assets/images/delete.svg"
+import editImage from "../../assets/images/edit.svg"
+import { editActive } from "../../features/transaction/transactionSlice"
 
-const Transaction = ({ transaction }) => {
+export default function Transaction({ transaction }) {
     const { name, amount, type } = transaction || {}
+    const dispatch = useDispatch()
+
+    const handleEdit = () => {
+        dispatch(editActive(transaction))
+    }
+
     return (
         <li className={`transaction ${type}`}>
             <p>{name}</p>
             <div className="right">
                 <p>৳ {amount}</p>
-                <button className="link">
-                    <img
-                        className="icon"
-                        alt='icon-img' src={editImage}
-                    />
+                <button className="link" onClick={handleEdit}>
+                    <img alt="Edit" className="icon" src={editImage} />
                 </button>
                 <button className="link">
-                    <img
-                        className="icon"
-                        alt='icon-img' src={deleteImage}
-                    />
+                    <img alt="Delete" className="icon" src={deleteImage} />
                 </button>
             </div>
         </li>
     )
 }
-
-export default Transaction
--- src/components/transactions/Transactions.js
+++ src/components/transactions/Transactions.js
@@ -1,36 +1,43 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import Transaction from './Transaction'
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchTransactions } from "../../features/transaction/transactionSlice"
+import Transaction from "./Transaction"
 
-const Transactions = () => {
-    const { transactions, isLoading, isError, error } = useSelector(state => state.transaction)
+export default function Transactions() {
+    const dispatch = useDispatch()
 
-    //decide what to render
+    const { transactions, isLoading, isError } = useSelector(
+        (state) => state.transaction
+    )
+
+    useEffect(() => {
+        dispatch(fetchTransactions())
+    }, [dispatch])
+
+    // decide what to render
     let content = null
-    if (isLoading) content = <p>Loading....</p>
+    if (isLoading) content = <p>Loading...</p>
 
-    if (!isLoading && isError) content = <p className="error">There was an error occured</p>
+    if (!isLoading && isError)
+        content = <p className="error">There was an error occured</p>
 
     if (!isLoading && !isError && transactions?.length > 0) {
-        content = transactions.map(transaction => <Transaction key={transaction.id} transaction={transaction} />)
+        content = transactions.map((transaction) => (
+            <Transaction key={transaction.id} transaction={transaction} />
+        ))
     }
 
     if (!isLoading && !isError && transactions?.length === 0) {
-        content = <p>No transaction found!</p>
+        content = <p>No transactions found!</p>
     }
-
 
     return (
         <>
             <p className="second_heading">Your Transactions:</p>
 
             <div className="conatiner_of_list_of_transactions">
-                <ul>
-                    {content}
-                </ul>
+                <ul>{content}</ul>
             </div>
         </>
     )
 }
-
-export default Transactions
